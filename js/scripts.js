f9ceef4d5a1f6c3c0499cc664ce0060408cbb9aa
bdharma <balasubramanian_dharmaraj@trimble.com>
2022-01-27 11:47:47 +0530

    All the required files of card component

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//CARD
+const changeVariant1 = () => {
+    const element = document.getElementById("variant");
+    element.classList.remove("variant2");
+}
+const changeVariant2 = () => {
+    let element = document.getElementById("variant");
+    element.classList.add("variant2");
+}
