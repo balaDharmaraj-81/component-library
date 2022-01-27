75f62825f741b8ac2495ed24217805213d4dfa53
bdharma <balasubramanian_dharmaraj@trimble.com>
2022-01-28 00:46:34 +0530

    Card component updated

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-//CARD
-const changeVariant1 = () => {
-    const element = document.getElementById("variant");
-    element.classList.remove("variant2");
-}
-const changeVariant2 = () => {
-    let element = document.getElementById("variant");
-    element.classList.add("variant2");
-}
